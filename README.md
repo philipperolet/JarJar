6558364c1ae5e88055b07c40610c98da25912a3b
sittingbull <pr@little-big-data.com>
2016-12-27 21:27:13 +0100

    Logs in and gets last order amount
    Using selenium + python rather than
    JS + phantomJS

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # JarJar
 
 ## Ongoing todo
-1. Autoexec / compile phantomJS
+1. A script
 a. that says hello world
 b. that reads the monoprix page title
-*c. that clicks on MonCompte
+c. that clicks on MonCompte
+d. that logs in
+e. that displays an info about amount of last order
+* f. with almost clean code, logging when needed
 
+- that's headless
+- with timings if stuff takes too long
+- that waits correclty before logging
+
+
+- last visited unseen buffer on emacs
+- that checks if you're logged in
--- hello.py
+++ hello.py
@@ -1,32 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
+LOAD_WT = 10  # Waiting time for elements to load / show (secs)
+driver = webdriver.Chrome()
 
-# go to the google home page
-driver.get("http://www.google.com")
 
-# the page is ajaxy so the title is originally this:
-print driver.title
+class MonopBot(object):
+    """
+    Bot class that can perform various actions as a
+    visitor of monoprix.fr
+    """
+    def __init__(self):
+        # to avoid elements not found because not yet loaded, wait for some time before timing out
+        driver.implicitly_wait(LOAD_WT)
+        driver.get("http://www.monoprix.fr")
 
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
+    def login(self):
+        monCompte = driver.find_element_by_css_selector(".dropdown.compte")
+        monCompte.click()
+        # enters login / pass and logs in
+        login_field = driver.find_element_by_id("emailPopin")
+        try:
+            WebDriverWait(driver, LOAD_WT).until(EC.visibility_of(login_field))
+        except TimeoutException:
+            driver.quit()
+            raise RuntimeError("Could not log in")
+        login_field.click()
+        login_field.send_keys('philipperolet@gmail.com')
+        pass_field = driver.find_element_by_id("mdpPopin")
+        pass_field.send_keys('stuff6472!')
+        driver.find_element_by_css_selector('#loginForm .modal-action button').click()
+        driver.find_element_by_css_selector(
+            'li.dropdown.connect ul.dropdown-menu.compte'
+        )  # Once found, it means we're logged in
 
-# type in the search
-inputElement.send_keys("cheese!")
+    def get_last_order_amount(self):
+        driver.get('https://www.monoprix.fr/jsp/account/accountOrders.jsp')
+        return driver.find_element_by_css_selector(
+            "table.commandes-related tbody tr:first-child td:first-child+td+td+td"
+        ).text
 
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-    # You should see "cheese! - Google Search"
-    print driver.title
-
-finally:
-    driver.quit()
+bot = MonopBot()
+bot.login()
+print u'Montant dernière commande : {}'.format(bot.get_last_order_amount())
+driver.quit()
